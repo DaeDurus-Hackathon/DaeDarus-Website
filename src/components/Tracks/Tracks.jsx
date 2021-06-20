0b3f2fd3767d8a7e650c624a43f9796c73a15e03
Arnab Chatterjee <arnabc1701@gmail.com>
2021-06-20 12:05:22 +0530

    Tracks SVGs added

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
 import '../Tracks/Tracks.css'
+import love from '../../DrawKit - Love & Family/family2.svg'
+import open from '../../DrawKit - Love & Family/family6.svg'
+import health from '../../DrawKit - Love & Family/family3.svg'
+import green from '../../DrawKit - Love & Family/family8.svg'
+
 
 export default function Tracks() {
     return (
@@ -14,6 +19,7 @@ export default function Tracks() {
                             <div class="flip-card-front">
                             <h1 className="mainhead1">Track 1:</h1>
                             <h1 className="mainhead1">Diversity and Inclusion</h1>
+                            <img className="svg-img" src={love} height="80%" width = "70%"/>
                             </div>
                             <div class="flip-card-back">
                             <h4>Create Technology to help create awareness and ultimately help in the inclusion of the less identified minor communities like LGBTQIA+, WIT, etc., establishing a sense of belonging among people coming from different walks of life.</h4>
@@ -27,6 +33,7 @@ export default function Tracks() {
                             <div class="flip-card-front">
                             <h1 className="mainhead1">Track 2:</h1>
                             <h1 className="mainhead1">Open Innovation</h1>
+                            <img className="svg-img" src={open} height="80%" width = "70%"/>
                             </div>
                             <div class="flip-card-back">
                             <h4>Technology isn’t restrictive, and neither are we. Even if you have an idea that does not fit in our primary themes, you’re welcome to portray any idea that creates a good social impact on society in any manner.</h4>
@@ -42,6 +49,7 @@ export default function Tracks() {
                             <div class="flip-card-front">
                             <h1 className="mainhead1">Track 3:</h1>
                             <h1 className="mainhead1">Health Care Tech</h1>
+                            <img className="svg-img" src={health} height="80%" width = "70%"/>
                             </div>
                             <div class="flip-card-back">
                             <h4>Amidst the world in a pandemic, the healthcare systems of every country have been overburdened. With several avenues open for exploration and research, come up and dominate the world with ideas to boost the healthcare sector’s productivity. Give new insights to improve the overall quality of care provided. </h4>
@@ -55,6 +63,7 @@ export default function Tracks() {
                             <div class="flip-card-front">
                             <h1 className="mainhead1">Track 4:</h1>
                             <h1 className="mainhead1">Green Tech</h1>
+                            <img className="svg-img" src={green} height="80%" width = "70%"/>
                             </div>
                             <div class="flip-card-back">
                             <h4>To revolutionize the way technology is being molded, as it has great effects on the world’s effort of a greener and richer environment. Bring your innovative minds to work towards a greener future and a sustainable environment.</h4>
