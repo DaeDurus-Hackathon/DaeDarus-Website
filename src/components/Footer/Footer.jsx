e1c2eebe12d332b6067a3da73e64cbec9bd29383
SubhayanDas08 <nips.subhayan@gmail.com>
2021-06-27 21:39:58 +0530

    Small Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from 'react-bootstrap'
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import FooterBackground from "../FooterBackground/FooterBackground";
+import Logo1 from '../../images/Logo1.png'
 
 import '../Footer/footer.css'
 
@@ -10,20 +11,23 @@ export default function Footer() {
 
     <div className="footer" id="footer">
       {/* <FooterBackground/> */}
-      <Container className="py-5">
+      {/* <Container className="py-5"> */}
       <h1 className="grad-text">Contact us</h1>
         <MDBFooter color="blue" className="font-small pt-4 mt-4">
           <MDBContainer fluid className="text-center text-md-left">
             <MDBRow>
-              <MDBCol md="8">
-                <h3 className="title">Microsoft Student Ambassador Club</h3>
+              <MDBCol md="2.5">
+                <img id="comlogo" className="com-logo" src={Logo1}/>
+              </MDBCol>
+              <MDBCol md="6">
+                <h3 className="title">Microsoft Student Community KIIT</h3>
                 <p>
                   Finding your community is more important than ever as classes
                   and social activities take place virtually.
                 </p>
               </MDBCol>
-              <MDBCol md="2">
-                <h3 className="title">Connect</h3>
+              <MDBCol md="4">
+                <h3 className="title">Connect with us at: </h3>
                 <ul>
                   <li className="list-unstyled">
                     <a href="#!">Link 1</a>
@@ -42,11 +46,11 @@ export default function Footer() {
           <div className="footer-copyright text-center py-3">
             <MDBContainer fluid>
               &copy; {new Date().getFullYear()} Copyright:{" "}
-              <a href="https://msackiit.tech/" target="__blank"> MSAC KIIT </a>
+              <a href="https://msackiit.tech/" target="__blank"> MSC KIIT </a>
             </MDBContainer>
           </div>
         </MDBFooter>
-      </Container>
+      {/* </Container> */}
       
     </div>
   );
