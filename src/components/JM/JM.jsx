1e3e51630d7e83596ae49e0182057a7a93142c42
Arnab Chatterjee <arnabc1701@gmail.com>
2021-07-11 17:21:23 +0530

    Links fixed

--- src/components/JM/JM.jsx
+++ src/components/JM/JM.jsx
@@ -34,8 +34,8 @@ export default function JM() {
 
             <h1 data-aos="zoom-in" data-aos-duration="300" data-aos-easing="ease-in-sine" ><span className="text-partner">Mentors</span></h1>
             <Row className="my-5">
-                <Col><img class="speak-logos" src={Rishit}/><h2>Rishit Dagli</h2><SocialIcon url="https://www.linkedin.com/in/mishra-aanchal/" target="_blank"/></Col>
-                <Col><img class="speak-logos" src={Pratyush}/><h2>Pratyush Agarwal</h2><SocialIcon url="https://www.linkedin.com/in/bhargavjoshi55/" target="_blank"/></Col>
+                <Col><img class="speak-logos" src={Rishit}/><h2>Rishit Dagli</h2><SocialIcon url="https://www.linkedin.com/in/rishit-dagli/" target="_blank"/></Col>
+                <Col><img class="speak-logos" src={Pratyush}/><h2>Pratyush Agarwal</h2></Col>
                 <Col><img class="speak-logos" src="https://media-exp1.licdn.com/dms/image/C4E03AQG0mtRxvZyNSA/profile-displayphoto-shrink_400_400/0/1608149412407?e=1631750400&v=beta&t=AwgapwRfkLH81eFVjvqa8e0mokW4Y3xvoHn6cTfHk5k"/><h2>Rajatav Dutta</h2><SocialIcon url="https://www.linkedin.com/in/williams-k-sarah/" target="_blank"/></Col>
             </Row>
 
