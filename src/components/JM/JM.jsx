0ce1d12bbfb2277a64adc913e53aa14fabb59186
Akash Chouhan <akash.c1500@gmail.com>
2021-07-17 22:46:08 +0530

    judges update

--- src/components/JM/JM.jsx
+++ src/components/JM/JM.jsx
@@ -11,8 +11,9 @@ import Ronak from '../../jm-pictures/Judges/Ronak Sahuja.jpg'
 import Jen from '../../jm-pictures/Judges/Jen Looper.jpg'
 
 
-import Rishit from '../../jm-pictures/Judges/Rishit Dagil.jpg'
+// import Rishit from '../../jm-pictures/Judges/Rishit Dagil.jpg'
 import Pratyush from '../../jm-pictures/Speakers/Pratyush Agrawal.jpg'
+import AmanMittal from '../../jm-pictures/Judges/Aman Mittal.jpg'
 
 export default function JM() {
     return (
@@ -27,6 +28,7 @@ export default function JM() {
                 <Col><img class="speak-logos" src={Sarah}/><h2>Sarah Williams</h2><SocialIcon url="https://www.linkedin.com/in/williams-k-sarah/" target="_blank"/></Col>
             </Row>
             <Row className="my-5">
+            <Col><img class="speak-logos" src={AmanMittal}/><h2>Aman Mittal</h2><SocialIcon url="https://www.linkedin.com/in/mishra-aanchal/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src={Ronak}/><h2>Ronak Sahuja</h2><SocialIcon url="https://www.linkedin.com/in/ronaksakhuja/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src={Jen}/><h2>Jen Looper</h2><SocialIcon url="https://www.linkedin.com/in/jen-looper-3442413/" target="_blank"/></Col>
             </Row>
@@ -34,7 +36,6 @@ export default function JM() {
 
             <h1 data-aos="zoom-in" data-aos-duration="300" data-aos-easing="ease-in-sine" ><span className="text-partner">Mentors</span></h1>
             <Row className="my-5">
-                <Col><img class="speak-logos" src={Rishit}/><h2>Rishit Dagli</h2><SocialIcon url="https://www.linkedin.com/in/rishit-dagli/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src={Pratyush}/><h2>Pratyush Agarwal</h2><SocialIcon url="https://www.linkedin.com/in/pratsy/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src="https://media-exp1.licdn.com/dms/image/C4E03AQG0mtRxvZyNSA/profile-displayphoto-shrink_400_400/0/1608149412407?e=1631750400&v=beta&t=AwgapwRfkLH81eFVjvqa8e0mokW4Y3xvoHn6cTfHk5k"/><h2>Rajatav Dutta</h2><SocialIcon url="https://www.linkedin.com/in/rajatavdutta/" target="_blank"/></Col>
             </Row>
