c7a8c3cb5cbd9dd3a58c4d1fb83e104c92f940b0
Arnab Chatterjee <arnabc1701@gmail.com>
2021-07-25 10:08:45 +0530

    judges added

--- src/components/JM/JM.jsx
+++ src/components/JM/JM.jsx
@@ -9,6 +9,8 @@ import Bhargav from '../../jm-pictures/Judges/Bharjav Joshi.jpg'
 import Sarah from '../../jm-pictures/Judges/Sarah Williams.jpg'
 import Ronak from '../../jm-pictures/Judges/Ronak Sahuja.jpg'
 import Jen from '../../jm-pictures/Judges/Jen Looper.jpg'
+import anna from '../../jm-pictures/Judges/anna.jpeg'
+import sndeep from '../../jm-pictures/Judges/sndeep.jpeg'
 
 
 import Rishit from '../../jm-pictures/Judges/Rishit Dagil.jpg'
@@ -26,13 +28,18 @@ export default function JM() {
                 <Col><img class="speak-logos" src={Aanchal}/><h2>Aanchal Mishra</h2><SocialIcon url="https://www.linkedin.com/in/mishra-aanchal/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src={Bhargav}/><h2>Bhargav Joshi</h2><SocialIcon url="https://www.linkedin.com/in/bhargavjoshi55/" target="_blank"/></Col>
                 <Col><img class="speak-logos" src={Sarah}/><h2>Sarah Williams</h2><SocialIcon url="https://www.linkedin.com/in/williams-k-sarah/" target="_blank"/></Col>
-                
             </Row>
             <Row className="my-5">
             <Col><img class="speak-logos" src={Jen}/><h2>Jen Looper</h2><SocialIcon url="https://www.linkedin.com/in/jen-looper-3442413/" target="_blank"/></Col>
             <Col><img class="speak-logos" src={AmanMittal}/><h2>Aman Mittal</h2><SocialIcon url="https://www.linkedin.com/in/aman-mittal-05a239117/" target="_blank"/></Col>
             <Col><img class="speak-logos" src={Rishit}/><h2>Rishit Dagli</h2><SocialIcon url="https://www.linkedin.com/in/rishit-dagli/" target="_blank"/></Col>
-                <Col><img class="speak-logos" src={Ronak}/><h2>Ronak Sahuja</h2><SocialIcon url="https://www.linkedin.com/in/ronaksakhuja/" target="_blank"/></Col>
+            
+            </Row>
+
+            <Row className="my-5">
+            <Col><img class="speak-logos" src={Ronak}/><h2>Ronak Sahuja</h2><SocialIcon url="https://www.linkedin.com/in/ronaksakhuja/" target="_blank"/></Col>
+                <Col><img class="speak-logos" src={anna}/><h2>Anna Godfrey</h2><SocialIcon url="https://www.linkedin.com/in/anna-godfrey-7b79485a/" target="_blank"/></Col>
+                <Col><img class="speak-logos" src={sndeep}/><h2>Sandeep Singh</h2><SocialIcon url="https://www.linkedin.com/in/sandybcsald/" target="_blank"/></Col>
             </Row>
 
 
