1d3b3f63802158889e43a9b0b37108ed2b3950c7
Arnab Chatterjee <arnabc1701@gmail.com>
2021-07-31 18:43:00 +0530

    sponsors and cp added

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -3,6 +3,8 @@ import { Container, Col, Row, Button } from 'react-bootstrap'
 import '../Community/community.css'
 import CFCLogo_Dark from '../../community-partner-logos/CFCLogo_Dark.png'
 import techsahead from '../../community-partner-logos/techsahead.png'
+import hcvit from '../../community-partner-logos/hcvit.png'
+import hnitr from '../../community-partner-logos/hnitr.png'
 
 export default function Community() {
     return (
@@ -17,13 +19,11 @@ export default function Community() {
                 <Col><img id="comlogo" className="com-logo" src={techsahead}/><h4>Techsahead</h4></Col>
                 {/* <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col> */}
             </Row>
-            {/* <Row className="my-5">
-                <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
-                <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
-                <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
-                <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
-            </Row>
             <Row className="my-5">
+                <Col><img id="comlogo" className="com-logo" src={hcvit}/><h4>Hackclub VIT B</h4></Col>
+                <Col><img id="comlogo" className="com-logo" src={hnitr}/><h4>Hack NIT R</h4></Col>
+            </Row>
+            {/* <Row className="my-5">
                 <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
                 <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
                 <Col><img id="comlogo" className="com-logo" src="https://media-exp3.licdn.com/dms/image/C4E0BAQHikN6EXPd23Q/company-logo_200_200/0/1595359131127?e=2159024400&v=beta&t=S5MNjBDjiH433VCWzjPeiopNDhxGwmfcMk4Zf1P_m_s"/><h4>KIIT Kaksha</h4></Col>
